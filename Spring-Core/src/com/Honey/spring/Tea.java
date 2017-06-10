247d876051a930ff07fc63b4f6f3c583f2bc294b
Honey <Honeyjn15@gmail.com>
2017-06-10 17:53:03 +0530

    Intial Checking for testing

---
+++ Spring-Core/src/com/Honey/spring/Tea.java
@@ -0,0 +1,13 @@
+package com.Honey.spring;
+
+public class Tea implements IHotDrink {
+
+	@Override
+	public void prepareHotTea() {
+		// TODO Auto-generated method stub
+		System.out.println("this is implemented  method in Tea class");
+		System.out.println("this is a GIT repository");
+	}
+
+	
+}
