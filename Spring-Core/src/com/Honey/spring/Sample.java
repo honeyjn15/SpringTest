51cdc8667a5f2b84d9459d0a4b5b7c1be9fc4390
honeyjn15 <honeyjn15@gmail.com>
2017-08-14 12:45:41 +0530

    Sample

---
+++ Spring-Core/src/com/Honey/spring/Sample.java
@@ -0,0 +1,13 @@
+package com.Honey.spring;
+
+public class Sample{
+
+
+	public void HotTea() {
+		// TODO Auto-generated method stub
+		System.out.println("this is implemented  method in Tea class");
+
+	}
+
+	
+}
